c89d4bdda94db8459d783e1ca3a14c2b65fb07f4
Dudenko Vadim <dudenko.vadim@gmail.com>
2021-05-07 13:20:23 +0500

    Changes

--- assets/js/mercury.js
+++ assets/js/mercury.js
@@ -1,5 +1,5 @@
 if (typeof mercuryParam != "undefined") {
-    var sdk = new MercurySDK({
+     sdk = new MercurySDK({
         checkoutUrl: "/wc-api/create_transaction",
         statusUrl: "/wc-api/status",
         checkStatusInterval: parseInt(mercuryParam.time, 2),
@@ -11,26 +11,26 @@ if (typeof mercuryParam != "undefined") {
             DASH: mercuryParam.dash
         }
     });
-}
-var status = false;
 
-jQuery(function(jQuery){
+    var status = false;
 
-    function successCallback(data) {
-        var checkoutForm = jQuery( "form.woocommerce-checkout");
+    jQuery(function(jQuery){
+
+        function successCallback(data) {
+            var checkoutForm = jQuery( "form.woocommerce-checkout");
             checkoutForm.find("#payment_method_mercury_validate").detach();
             checkoutForm.submit();
-    }
+        }
 
-    function qrRequest() {
-        var errorCount = jQuery(".woocommerce-error li").length;
+        function qrRequest() {
+            var errorCount = jQuery(".woocommerce-error li").length;
 
-        if (errorCount === 1) { // Validation Passed (Just the Fake Error I Created Exists)
-            jQuery(".woocommerce-error").detach();
-            jQuery( "html, body").stop();
-            let mail = jQuery("#billing_email").val(),
-                price = mercuryParam.cart_price,
-                currency = mercuryParam.currency;
+            if (errorCount === 1) { // Validation Passed (Just the Fake Error I Created Exists)
+                jQuery(".woocommerce-error").detach();
+                jQuery( "html, body").stop();
+                let mail = jQuery("#billing_email").val(),
+                    price = mercuryParam.cart_price,
+                    currency = mercuryParam.currency;
 
                 sdk.checkout(price, currency, mail);
 
@@ -41,36 +41,37 @@ jQuery(function(jQuery){
                     }
                 });
 
-        } else { // Validation Failed (Real Errors Exists, Remove the Fake One)
+            } else { // Validation Failed (Real Errors Exists, Remove the Fake One)
 
-            jQuery('.woocommerce-error li').each(function(){
-                var errorText = jQuery(this).find(".mercury_fake_error").text();
-                if (errorText === "mercury_fake_error"){
-                    jQuery(this).css("display", "none");
-                }
-            });
+                jQuery(".woocommerce-error li").each(function(){
+                    var errorText = jQuery(this).find(".mercury_fake_error").text();
+                    if (errorText === "mercury_fake_error"){
+                        jQuery(this).css("display", "none");
+                    }
+                });
+            }
+            //
+            return false;
         }
-        //
-        return false;
-    }
 
 
-    var checkoutForm = jQuery('form.woocommerce-checkout');
+        var checkoutForm = jQuery("form.woocommerce-checkout");
 
-    checkoutForm.on("checkout_place_order", function () {
-        if(status === "false" || status === false) {
-            if(jQuery("#payment_method_mercury").is(":checked")) {
-                checkoutForm.append("<input type='hidden' id='payment_method_mercury_validate' name='payment_method_mercury_validate'" +
-                    " value='1'>");
-            } else {
-                checkoutForm.find("#payment_method_mercury_validate").detach();
+        checkoutForm.on("checkout_place_order", function () {
+            if(status === "false" || status === false) {
+                if(jQuery("#payment_method_mercury").is(":checked")) {
+                    checkoutForm.append("<input type='hidden' id='payment_method_mercury_validate' name='payment_method_mercury_validate'" +
+                        " value='1'>");
+                } else {
+                    checkoutForm.find("#payment_method_mercury_validate").detach();
+                }
+                return true;
             }
             return true;
-        }
-        return true;
-    });
+        });
 
-    jQuery(document.body).on("checkout_error", qrRequest);
-});
+        jQuery(document.body).on("checkout_error", qrRequest);
+    });
+}
 
 
